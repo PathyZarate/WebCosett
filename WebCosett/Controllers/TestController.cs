dd3279b01dbe3f320f2c6f769f9a44605363af48
Patricia Zarate <solemayo081@gmail.com>
2025-03-18 08:41:13 -0400

    registro controller

---
+++ WebCosett/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebCosett.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpPost("procesar-archivo")]
+        public async Task<IActionResult> ProcesarArchivo(IFormFile archivo)
+        {
+           return Ok("HOLA");
+        }
+    }
+}
--- WebCosett/obj/Debug/net8.0/WebCosett.MvcApplicationPartsAssemblyInfo.cs
+++ WebCosett/obj/Debug/net8.0/WebCosett.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("WebCosett")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0c05dcad98dad5583d1fca180b547e0e17dd93a")]
-[assembly: System.Reflection.AssemblyProductAttribute("WebCosett")]
-[assembly: System.Reflection.AssemblyTitleAttribute("WebCosett")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
